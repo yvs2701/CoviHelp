a42e46342b9f4cd3f859738b3f8a2e4a5dc791d3
yvs2701 <yashvardhan.ys86@gmail.com>
2021-11-07 16:34:04 +0530

    Logic working TODO storage autorefresh notify user

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ const getRequest = async (URL) => {
         },
         mode: 'cors'
     }
-    const data = await fetch(URL, options);
-    return data.json();
-};
+    try {
+        const data = await fetch(URL, options);
+        return data.json();
+    } catch (err) {
+        console.error('Get request failed:\n' + err);
+        return { error_message: err };
+    }
+}
 
 const getStates = async () => {
     const url = 'https://cdn-api.co-vin.in/api/v2/admin/location/states';
@@ -37,14 +42,81 @@ const getDistricts = async (state) => {
     });
 }
 
+const filters = (arr) => {
+    const age = parseInt(document.getElementById('age-group-filter').value);
+    arr = arr.filter(obj => obj.min_age_limit >= age); // age filtered
+
+    /* const dose = document.getElementsByName('dose');
+    due to some warnings in the docs: https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByName
+    We can still use:*/
+    const dose = document.querySelector('input[name="vaccine"]:checked').value;
+    if (dose === '1')
+        arr = arr.filter(obj => obj.available_capacity_dose1 !== 0); // dose filtered
+    else if (dose === '1')
+        arr = arr.filter(obj => obj.available_capacity_dose2 !== 0); // dose filtered
+
+    const vaccine = document.querySelector('input[name="vaccine"]:checked').value;
+    if (vaccine) arr = arr.filter(obj => obj.vaccine === vaccine); // vaccine filtered
+
+    return arr;
+}
+
+const renderSlots = (response) => {
+    var table = document.getElementsByTagName('table');
+    if (table.length == 1) {
+        // till now we have only 1 table (i.e. no slots-table was generated)
+        table = document.createElement('table');
+        table.setAttribute('class', 'slots-table');
+        document.getElementById('submit').insertAdjacentElement("afterend", table);
+    } else {
+        table = table[1]; // assign table to the slots-table to rerender it with new data
+    }
+    if (response.sessions.length === 0) {
+        table.innerHTML = '<tr><td><h4>No slots found</h4></td></tr>';
+    } else {
+        const res = filters(response.sessions, table); // modify the response based on filters selected by user
+        if (res.length === 0) {
+            table.innerHTML = '<tr><td><h4>No slots found</h4></td></tr>';
+        } else {
+            table.innerHTML =
+                `<thead>
+                    <tr>
+                        <td><h4>Center Name</h4></td>
+                        <td><h4>Address</h4></td>
+                        <td><h4>Vaccine</h4></td>
+                        <td><h4>Age limit</h4></td>
+                        <td><h4>Vaccination slots</h4></td>
+                    </tr>
+                </thead>
+                <tbody>`;
+            res.forEach((center) => {
+                table.innerHTML +=
+                    `<tr>
+                        <td>${center.name}</td>
+                        <td>${center.address}</td>
+                        <td>${center.vaccine}</td>
+                        <td>${center.min_age_limit}</td>
+                        <td>
+                            <p><h4>Dose 1: </h4>${center.available_capacity_dose1}</p>
+                            <p><h4>Dose 2: </h4>${center.available_capacity_dose2}</p>
+                        </td>
+                    </tr>`;
+            });
+            table.innerHTML += '</tbody>';
+        }
+    }
+}
+
 const findVaccineByPincode = async (pin) => {
     const today = new Date();
+    /* String.padStart(): Pads the current string with a given string (possibly repeated) so that the
+    resulting string reaches a given length. The padding is applied from the start (left) of the current string. */
     const dd = String(today.getDate()).padStart(2, '0');
     const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     const yyyy = today.getFullYear();
     const response = await
         getRequest(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${dd}-${mm}-${yyyy}`);
-    // response.sessions: array...
+    renderSlots(response);
 }
 const findVaccineByState = async (dist_id) => {
     const today = new Date();
@@ -53,33 +125,9 @@ const findVaccineByState = async (dist_id) => {
     const yyyy = today.getFullYear();
     const response = await
         getRequest(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${dist_id}&date=${dd}-${mm}-${yyyy}`);
-    // response.sessions: [{}, {}, {}, ...]
+    renderSlots(response);
 }
-/** each object in array of response sessions looks like:
-{
-    address: "Chinhat Lucknow"
-    allow_all_age: true
-    available_capacity: 0
-    available_capacity_dose1: 0
-    available_capacity_dose2: 0
-    block_name: "Chinhat"
-    center_id: 889391
-    date: "06-11-2021"
-    district_name: "Lucknow"
-    fee: "0"
-    fee_type: "Free"
-    from: "10:00:00"
-    lat: 26
-    long: 81
-    min_age_limit: 18
-    name: "Chinhat CHC Mobile Team-2"
-    pincode: 226028
-    session_id: "f38274f1-0dfa-4288-8174-f47d3bed1e40"
-    slots: ["10:00AM-11:00AM", "11:00AM-12:00PM", "12:00PM-01:00PM", "01:00PM-04:00PM"]
-    state_name: "Uttar Pradesh"
-    to: "16:00:00"
-    vaccine: "COVAXIN"
-} */
+
 window.onload = () => {
     getStates();
     const stateRadio = document.getElementsByName('state-or-pin');
